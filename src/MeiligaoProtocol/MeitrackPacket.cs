77dc148f776898d4cc889f5e204281ba91f41ee7
brimzi <brimzi@gmail.com>
2014-08-10 00:56:16 +0200

    more changes:added classes for the new protocol

---
+++ src/MeiligaoProtocol/MeitrackPacket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeiligaoProtocol
+{
+    public class MeitrackPacket
+    {
+        public string Header { get; set; }
+
+        public string PacketFlag { get; set; }
+
+        public ushort Length { get; set; }
+
+        public string IMEI { get; set; }
+
+        public MeitrackCommand Command{ get; set;}
+
+        public MeitrackData Data { get; set; }
+
+        public ushort CheckSum { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/MeiligaoProtocol/MeitrackProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeiligaoProtocol
+{
+    class MeitrackProcessor
+    {
+        public static byte[] GetBytes(MeiligaoPacket Packet)
+        {
+            throw new NotImplementedException();
+        }
+        
+
+      
+
+        public static MeitrackPacket GetMeitrackPacket(byte[] bytes)
+        {
+            throw new NotImplementedException();
+        }
+        
+
+
+    }
+}
